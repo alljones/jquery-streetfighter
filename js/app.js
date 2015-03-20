70310905db499f8efe57f3bec4023d9b0c2b508b
Allan Jones <acreativej@gmail.com>
2015-03-20 16:11:58 -0500

    Added jQuery Interaction

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
+		//console.log('mousedown');
+		//play hadouken sound
+		//show hadouken
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			//animate it to the right of screen
+			{'left': '1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left','520px');
+			});
+	})
+	.mouseup(function(){
+		//console.log('mouseup');
+		//ryu goes back to his ready position
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
+	.keydown(function(){
+		$('.ryu-still').hide();
+		$('.ryu-cool').show();
+	});
+});
+
+function playHadouken () {
+	$('#hadouken-sound') [0].volume = 0.5;
+	$('#hadouken-sound') [0].load();
+	$('#hadouken-sound') [0].play();
+};
